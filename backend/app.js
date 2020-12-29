375d6b31a0da8513c06426b93c25ab6016a6fec3
aditya278 <aditya.shukla278@gmail.com>
2020-12-29 19:46:40 +0530

    Adding backend

---
+++ backend/app.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var logger = require('morgan');
+
+var usersRouter = require('./routes/users');
+
+var app = express();
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.use('/users', usersRouter);
+
+module.exports = app;
