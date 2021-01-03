ec761c2fbc255c84ab963150c8695766aa4df43f
Aditya Shukla <aditya.shukla278@gmail.com>
2021-01-03 20:06:43 +0530

    Update RegisterView.js

--- frontend/src/views/RegisterView.js
+++ frontend/src/views/RegisterView.js
@@ -21,8 +21,8 @@ function Copyright() {
   return (
     <Typography variant="body2" color="textSecondary" align="center">
       {'Copyright © '}
-      <Link color="inherit" href="https://material-ui.com/">
-        Your Website
+      <Link color="inherit" href="https://iadityashukla.com/">
+        Aditya Shukla
       </Link>{' '}
       {new Date().getFullYear()}
       {'.'}
