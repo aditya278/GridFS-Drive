c136659cb0cf80bcef929951c5caad0e51ff0f50
Aditya Shukla <aditya.shukla278@gmail.com>
2021-01-03 22:54:03 +0530

    Update jenkins-script.sh

--- jenkins-script.sh
+++ jenkins-script.sh
@@ -1,10 +1,9 @@
-sudo pm2 stop all
-sudo git pull
+sudo pm2 delete GridFS-Drive
 cd frontend/
 npm install
 npm run build
 cd ..
 cd backend/
 npm install
+sudo pm2 ./bin/www --name "GridFS-Drive"
 cd ..
-sudo pm2 restart all
