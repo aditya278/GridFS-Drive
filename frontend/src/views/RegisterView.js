de0010a7cc1f2bb1db31bdcada1b8d7dfbfcb490
Aditya Shukla <aditya.shukla278@gmail.com>
2021-01-03 20:10:51 +0530

    Jenkin's Test #2

--- frontend/src/views/RegisterView.js
+++ frontend/src/views/RegisterView.js
@@ -22,7 +22,7 @@ function Copyright() {
     <Typography variant="body2" color="textSecondary" align="center">
       {'Copyright © '}
       <Link color="inherit" href="https://iadityashukla.com/">
-        Aditya Shukla
+        Aditya
       </Link>{' '}
       {new Date().getFullYear()}
       {'.'}
